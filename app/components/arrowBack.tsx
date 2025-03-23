6818a941c1a5e1abf63473aa81429621d8512532
JoseMatheus29 <mateus.alvino.101@gmail.com>
2025-03-23 13:00:23 -0300

    Fix: Atualizando componente de seta

--- app/components/arrowBack.tsx
+++ app/components/arrowBack.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
-import Svg, { Path } from 'react-native-svg';
+import {  StyleSheet, TouchableOpacity } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -12,13 +12,8 @@ const ArrowBack = () => {
         navigation.goBack();
     };
     return (
-        <TouchableOpacity onPress={handleVoltar}>
-            <View style={styles.arrowBackIcon}>
-                <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <Path d="M21.1431 12L2.8569 12" stroke="#3161B2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                    <Path d="M9.85693 5L2.85693 12L9.85693 19" stroke="#3161B2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </Svg>
-            </View>
+        <TouchableOpacity>
+            <Icon name="arrow-back" size={24} color="#3161B2" />
         </TouchableOpacity>
 
     );
